d0d6571dfd8cc2ffe46b3de038384f6cb1ff7213
Allen Zhong <zhong.allen@gmail.com>
2017-12-05 21:53:43 +1300

    Change description and clean code

--- spec/urban_dict_api_spec.rb
+++ spec/urban_dict_api_spec.rb
@@ -4,8 +4,4 @@ RSpec.describe UrbanDictApi do
   it "has a version number" do
     expect(UrbanDictApi::VERSION).not_to be nil
   end
-
-  it "does something useful" do
-    expect(false).to eq(true)
-  end
 end
