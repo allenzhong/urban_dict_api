8501bbce4463ce9d928bd02cad23e59178964163
allenzhong <zhong.allen@gmail.com>
2017-12-19 13:54:34 +1300

    add query method

--- lib/urban_dict_api.rb
+++ lib/urban_dict_api.rb
@@ -2,5 +2,7 @@ require "urban_dict_api/version"
 require "urban_dict_api/api_helper"
 
 module UrbanDictApi
-  # Your code goes here...
+  def query(word)
+    ApiHelper.fetch_word(word)
+  end
 end
--- spec/urban_dict_api_spec.rb
+++ spec/urban_dict_api_spec.rb
@@ -1,7 +1,25 @@
 require "spec_helper"
-
+require 'urban_dict_api/word'
 RSpec.describe UrbanDictApi do
   it "has a version number" do
     expect(UrbanDictApi::VERSION).not_to be nil
+  end
+
+  describe "searching valid word" do
+    class DummyClass
+    end
+
+    before do
+      @dummy_class = DummyClass.new
+      @dummy_class.extend(UrbanDictApi)
+      word = UrbanDictApi::Word.new
+      allow(UrbanDictApi::ApiHelper).to receive(:fetch_word).and_return(word)
+    end
+
+    it 'should return a word model' do
+      query_word = "value"
+      return_word = @dummy_class.query(query_word)
+      expect(return_word).to be_truthy
+    end
   end
 end
