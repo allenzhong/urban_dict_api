2fd4e98717fbe47b562d8f555f1f1d9843db4506
allenzhong <zhong.allen@gmail.com>
2017-12-21 15:35:15 +1300

    add test for api

--- lib/urban_dict_api.rb
+++ lib/urban_dict_api.rb
@@ -1,8 +1,14 @@
 require 'urban_dict_api/version'
 require 'urban_dict_api/api_helper'
-
+require 'byebug'
 module UrbanDictApi
-  def query(word)
-    ApiHelper.fetch_word(word)
+  def query(value)
+    response = ApiHelper.fetch_word(value)
+    word = UrbanDictApi::Word.new { @value = value }
+    unless response[:result_type] == 'no_results'
+      word.tags = response[:tags]
+      word.definitions = response[:definitions]
+    end
+    word
   end
 end
--- spec/urban_dict_api_spec.rb
+++ spec/urban_dict_api_spec.rb
@@ -13,17 +13,49 @@ RSpec.describe UrbanDictApi do
       DummyClass.new
     end
 
-    let(:word) { UrbanDictApi::Word.new { @value = 'value' } }
-
-    before do
-      dummy_class.extend(UrbanDictApi)
-      allow(UrbanDictApi::ApiHelper).to receive(:fetch_word).and_return(word)
+    let(:word_no_result) { UrbanDictApi::Word.new { @value = 'value' } }
+    let(:response_no_result)  do 
+      { tags: [], definition: [], result_type: 'no_results' }
     end
 
-    it 'should return a word model' do
-      query_word = 'value'
-      return_word = dummy_class.query(query_word)
-      expect(return_word).to eq(word)
+    let(:word_has_result) do
+      UrbanDictApi::Word.new do
+        @value = 'value'
+        @tags = ['tag1']
+        @definitions = ['definition1']
+      end
+    end
+    let(:response_has_result)  do 
+      { tags: ['tag1'], definitions: ['definition1'], result_type: 'exact' }
+    end
+
+    context 'no result' do
+      before do
+        dummy_class.extend(UrbanDictApi)
+        allow(UrbanDictApi::ApiHelper).to receive(:fetch_word).and_return(response_no_result)
+      end
+
+      it 'should return a proper response' do
+        query_word = 'value'
+        return_word = dummy_class.query(query_word)
+        expect(return_word.value).to eq(word_no_result.value)
+      end
+    end
+
+    context 'has results' do
+      before do
+        dummy_class.extend(UrbanDictApi)
+        allow(UrbanDictApi::ApiHelper).to receive(:fetch_word).and_return(response_has_result)
+      end
+
+      it 'should return a proper response' do
+        query_word = 'value'
+        return_word = dummy_class.query(query_word)
+        puts word_has_result.value
+        expect(return_word.value).to eq(word_has_result.value)
+        expect(return_word.tags).to eq(word_has_result.tags)
+        expect(return_word.definitions).to eq(word_has_result.definitions)
+      end
     end
   end
 end
